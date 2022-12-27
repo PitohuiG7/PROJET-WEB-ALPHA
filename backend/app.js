dad05cdd75107e59b3c96bb913289a8d9cc6f5f4
Maimouna Timera <maimouna.timera@etu.univ-tours.fr>
2022-12-27 20:23:18 +0100

    Ajout de passport

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,9 @@
 const express =require('express')
-const sequelize = require('./model/Sequelize');
+const sequelize = require('./model/sequelize');
+
+const passport = require('passport');
+
+
+
+const app = express();
+app.listen(3000);
---
+++ backend/model/sequelize.js
@@ -0,0 +1,64 @@
+const {Sequelize,DataTypes} = require("sequelize");
+
+//definition des informations sur ma base de données
+const sequelize = new Sequelize(
+    'progwebserveur',
+    'root',
+    '',
+    {
+    	host:'localhost',
+    	dialect:'mysql'
+    }
+	);
+//verification de la connexion avec la base de données
+  sequelize.authenticate().then(()=>{
+	console.log('Connection has been established successfully');
+
+}).catch((error)=>{
+	console.log('Unable to connect to the database: ',error);
+});
+
+//création de ma table dans ma base de données
+ const users = sequelize.define('users',{
+ 	idUSer:{
+ 		type: DataTypes.INTEGER,
+ 		 autoIncrement:true,
+ 		 primaryKey:true,
+ 		 allowNull:false,
+ 	},
+ 	email:{
+ 		type : DataTypes.STRING(45),
+ 		allowNull:false,
+ 		primaryKey:true
+ 	},
+ 	pseudo:{
+ 		type : DataTypes.STRING(45),
+ 		allowNull:false,
+ 		primaryKey:true
+ 	},
+ 	name:{
+ 		type : DataTypes.STRING(45),
+ 		allowNull:false,
+ 		
+ 	},
+ 	lastName:{
+ 			type : DataTypes.STRING(45),
+ 		allowNull:false,
+ 	},
+ 	password:{
+ 			type : DataTypes.STRING(45),
+ 		allowNull:false,
+ 	},
+ 	isAdmin:{
+ 		type:DataTypes.TINYINT,
+  
+ 	}
+ })
+
+ sequelize.sync().then(()=>{
+ 	console.log('Users table created successfully');
+ }).catch((error)=>{
+ 	console.error('Unable to create table : ',error);
+ })
+
+module.exports = users;
